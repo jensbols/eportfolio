2b7ccb737c6db17d6be4d14413ffb7d2fea9bbc1
Jens Bols <jens.bols@student.hogent.be>
2022-02-25 13:23:14 +0100

    added basic hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,16 @@
+import face from '../images/samplefaceshot.jpg';
+
 export default function HeroSection() {
   return (
-    <div>
-      <h1>Hero Section Placeholder</h1>
+    <div className="min-h-screen bg-blue-100 p-32 text-center">
+      <h1 className='text-5xl font-bold text-blue-700 mb-2'>Hi i'm Jens</h1>
+      <p className='text-gray-700 text-lg mb-12'>I'm a computer science student located in Gent, Belgium</p>
+
+      <div>
+        <a href="#">My projects</a>
+        <a href="#">Contact</a>
+      </div>
+
     </div>
   )
 }

8128449833399db4fcea8ccfd1cee98b7aa1e457
Jens Bols <jens.bols@student.hogent.be>
2022-02-25 23:31:20 +0100

    added some bad background for testing purposes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,13 @@
-import face from '../images/samplefaceshot.jpg';
-
+import bgimage from '../images/bgyello.jpg'
 export default function HeroSection() {
   return (
-    <div className="min-h-screen bg-blue-100 p-32 text-center">
-      <h1 className='text-5xl font-bold text-blue-700 mb-2'>Hi i'm Jens</h1>
-      <p className='text-gray-700 text-lg mb-12'>I'm a computer science student located in Gent, Belgium</p>
+    <div style={{ backgroundImage: `url(${bgimage})` }} className="min-h-screen bg-blue-100 p-32 text-center px-96"> 
+      <h1 className='text-5xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-gray-300'>Hi i'm Jens</h1>
+      <p className='text-gray-700 text-lg mb-8'>I'm a computer science student located in Gent, Belgium</p>
 
-      <div>
-        <a href="#">My projects</a>
-        <a href="#">Contact</a>
+      <div className="space-x-2">
+        <a href="#" className="py-3 px-8 bg-gray-400 text-gray-800 rounded-lg hover:bg-gray-300 hover:shadow-xl hover:text-gray-900 transition duration-300">My projects</a>
+        <a href="#" className="py-3 px-8 bg-yellow-400 text-yellow-900 rounded-lg hover:bg-yellow-300 hover:shadow-xl transition duration-300">Contact</a>
       </div>
 
     </div>

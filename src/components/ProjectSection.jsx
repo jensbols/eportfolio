9d7d829484da65325e6c289460d032cd6d27c704
Jens Bols <jens.bols@student.hogent.be>
2022-02-27 11:47:37 +0100

    added project section basic outline of style

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,7 +1,16 @@
 export default function ProjectSection() {
   return (
-    <div>
-      <h1>Project Section</h1>
-    </div>
+    <>
+      <div className="max-w-7xl mx-auto flex bg-red-500 pt-32 pb-20 text-center">
+        {/* left */}
+        <div className="w-2/3 bg-red-500">
+          <h3>TEST</h3>
+        </div>
+        {/* right */}
+        <div className="w-1/3 bg-green-500">
+          <h3>Github</h3>
+        </div>
+      </div>
+    </>
   )
 }

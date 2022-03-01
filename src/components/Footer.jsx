74f9bf9700ebe6331b41f3d9e7f523d2bff0e730
Jens Bols <jens.bols@student.hogent.be>
2022-03-01 10:04:33 +0100

    added basic footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer section</h1>
-    </div>
+    <footer className="max-w-7xl mx-auto bg-gray-300">
+      <div className="py-4 border-b-4 border-blue-500 space-x-6 flex justify-center mx-10">
+        <a className="text-2xl font-light" href="#">Home</a>
+        <a className="text-2xl font-light" href="#">Skills</a>
+        <a className="text-2xl font-light" href="#">Projects</a>
+      </div>
+      <div className="mt-5 mx-5 text-center pb-10">
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </div>
+    </footer>
   )
 }

a5d427c2a72ffb19e6154815896b6e28fb9547fc
Jens Bols <jens.bols@student.hogent.be>
2022-03-02 17:30:28 +0100

    forgot border on services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -44,7 +44,7 @@ export default function Services() {
           <ServiceListItem itemName="Swagger" />
           <ServiceListItem itemName="GraphQL" />
         </div>
-        <div className="rounded-xl w-1/3 p-20 bg-gray-900 border-l-8 border-blue-500 px-14 py-10">
+        <div className="rounded-xl w-1/3 p-20 bg-gray-900 border-l-8 border-b-8 border-blue-500 px-14 py-10">
 
           <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16 text-white" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z" clip-rule="evenodd" />

71de18ee51a857f3b4403acf58e5fb411fe0ccd3
Jens Bols <jens.bols@student.hogent.be>
2022-03-02 20:26:08 +0100

    added styling to about me section and my skills section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,16 +3,18 @@ import aboutMeSvg from '../images/goal-svgrepo-com.svg';
 export default function AboutMe() {
   return (
     <>
-      <div className="max-w-7xl mx-auto flex pt-6 pb-20 px-32 bg-transparent">
-        {/* right */}
-        
-        {/* left */}
-        <div className="text-center mt-20 ml-10">
-          <h3 className='text-3xl font-bold text-center pt-3 border-b-4 border-blue-500 pb-3'>About me</h3>
-          <p className='mt-6 pb-6 border-b-4 border-blue-500'>I always had a hard time figuring out what I wanted to become in life, It wasn't until I enrolled in a computer course at my local highschool in Oostende that I found something that sparked my interest.
-          Further down the line in my college degree I found a passion for web development, not only front-end but also back-end. I also really like working with react and javascript based librarys and I hope to develop myself further in this branch.</p>
+      <div className='max-w-full bg-gray-900'>
+        <div className="max-w-7xl mx-auto flex pt-6 pb-20 px-32 bg-transparent">
+          {/* right */}
+
+          {/* left */}
+          <div className="text-center mt-10 ml-10">
+            <h3 className='text-3xl font-bold text-center pt-3 border-b-4 border-blue-500 pb-3 text-gray-200'>About me</h3>
+            <p className='mt-6 pb-6 border-b-4 border-blue-500 text-gray-400'>I always had a hard time figuring out what I wanted to become in life, It wasn't until I enrolled in a computer course at my local highschool in Oostende that I found something that sparked my interest.
+              Further down the line in my college degree I found a passion for web development, not only front-end but also back-end. I also really like working with react and javascript based librarys and I hope to develop myself further in this branch.</p>
+          </div>
+
         </div>
-        
       </div>
     </>
   )
--- src/components/SkillSetSection.jsx
+++ src/components/SkillSetSection.jsx
@@ -1,18 +1,19 @@
-import Skill from "./Skill";
 import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function SkillSetSection() {
   return (
     <>
-    <div className="max-w-7xl mx-auto px-10 mb-10">
-    <h2 className="text-3xl mb-3 font-bold">My Skills</h2>
-    <p className="mt-2 mb-1">HTML</p>
-    <ProgressBar completed={80} bgColor={"#3b82f6"}/>
-    <p className="mt-2 mb-1">CSS</p>
-    <ProgressBar completed={65} bgColor={"#3b82f6"}/>
-    <p className="mt-2 mb-1">Javascript</p>
-    <ProgressBar completed={70} bgColor={"#3b82f6"}/>
-    <p className="text-xs mt-5">More coming soon...</p>
+    <div className="bg-gray-200 py-10">
+      <div className="max-w-7xl mx-auto px-10">
+        <h2 className="text-3xl mb-3 font-bold">My Skills</h2>
+        <p className="mt-2 mb-1">HTML</p>
+        <ProgressBar completed={80} bgColor={"#111827"} />
+        <p className="mt-2 mb-1">CSS</p>
+        <ProgressBar completed={65} bgColor={"#111827"} />
+        <p className="mt-2 mb-1">Javascript</p>
+        <ProgressBar completed={70} bgColor={"#111827"} />
+        <p className="text-xs mt-5">More coming soon...</p>
+      </div>
     </div>
     </>
   )

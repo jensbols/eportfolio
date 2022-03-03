e8bdee75a128379bd253b9b7de74698af5a6f041
Jens Bols <jens.bols@student.hogent.be>
2022-03-03 21:43:01 +0100

    added some mock data

--- src/mock_data/blog-post-mock.js
+++ src/mock_data/blog-post-mock.js
@@ -1,9 +1,10 @@
 const BLOG_POST = [
   {
-    title: '',
-    text: '',
-    date: '',
-    imgUrl: '',
+    title: 'CSS with Tailwind',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    date: '3/03/2022',
+    imgUrl: '../images/tailwindblogpost.jpg',
   },
-  
 ]
+
+export {BLOG_POST};

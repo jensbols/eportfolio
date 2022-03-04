a9074f8d6748ef1e2501b24f1d4529fa5b6c31d6
Jens Bols <jens.bols@student.hogent.be>
2022-03-04 17:13:34 +0100

    added basic blog preview

--- src/components/BlogPostSummary.jsx
+++ src/components/BlogPostSummary.jsx
@@ -4,22 +4,22 @@ import { blogPreviewImages } from "../util/blogpreviewImages"
 export default function BlogPostSummary({title, text, date, icon}) {
   return (
     <>
-      <h2 className="text-center text-4xl my-10">Recent Posts</h2>
-      <div className="bg-gray-100">
+      <div className="bg-gray-100 mb-10">
         <div className="max-w-6xl mx-auto flex space-x-2">
           { /* image */}
-          <div className="w-1/3 border border-black ml-10">
-          <img src={blogPreviewImages[`${icon}`]} alt="blog post"></img>
+          <div className="w-1/3 border ml-10">
+          <img src={blogPreviewImages[`${icon}`]} alt="blog post" className="h-60 border-l-8 border-blue-500"></img>
           </div>
-          <div className="w-2/3 border border-black max-h-50 overflow-hidden">
+          <div className="w-2/3 border-black max-h-50 overflow-hidden">
             { /* right side */}
             <div className="flex justify-between">
 
             <h2 className="text-3xl mx-3 mt-3">{title}</h2>
             <p className="m-2">{date}</p>
             </div>
-            <div className="m-4 border border-black overflow-ellipsis">
+            <div className="m-4">
             <p className="">{text}</p>
+            <button className="float-right bg-blue-500 p-2 rounded-lg hover:bg-blue-400 transition duration-300">Read more</button>
             </div>
             
           </div>
--- src/components/BlogPostSummaryList.jsx
+++ src/components/BlogPostSummaryList.jsx
@@ -4,7 +4,8 @@ import BlogPostSummary from "./BlogPostSummary"
 export default function BlogPostSummaryList() {
   return (
     <>
-      <div>
+      <div className="bg-gray-100">
+      <h2 className="text-center text-4xl my-10">Recent Posts</h2>
         {BLOG_POST.map((blogPost) => (
           <BlogPostSummary key={blogPost.id} {...blogPost} />
         ))}
